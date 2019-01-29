ce655db0e409dcad9875ea27090ef1c6d690c7c0
MatthewSheppard1 <34612037+MatthewSheppard1@users.noreply.github.com>
2019-01-29 09:37:11 -0800

    Added minimum length of word feature

--- SentenceLength.py
+++ SentenceLength.py
@@ -4,10 +4,20 @@ import os
 def findAverageSentence(filePath, delimiters, minLength):
     file = open(filePath)
     textInFile = file.read()
-
+    print(minLength)
     words = textInFile.split(" ")
-    # print(words)
+    wordsToRemove = []
+    for i in words:
+        if(i.find(".") == -1):
+            if (len(i) < minLength):
+                wordsToRemove.append(i)
+        else:
+            if(len(i) - 1 < minLength):
+                wordsToRemove.append(i)
+    for i in wordsToRemove:
+        words.remove(i)
 
+    # print(words)
     sentences = textInFile.split(".")
     sentences.remove("")
 
@@ -28,4 +38,4 @@ minLength = eval(input("Enter the minimum length of a word (must be a positive i
 if (minLength < 1):
     minLength = 1
 
-print("The average sentence length is", findAverageSentence(userFile, userDelimeters, minLength))
+print("The average sentence length of words " + str(minLength) + " characters or more is ", findAverageSentence(userFile, userDelimeters, minLength))
