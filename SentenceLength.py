7d33075b117b0e8bf05bcf51d0409c4cae47af68
daryl.denaga <daryldenaga@gmail.com>
2019-01-30 08:05:38 -0800

    implemented multiple delimiters

--- SentenceLength.py
+++ SentenceLength.py
@@ -1,17 +1,28 @@
 import os
+import re
 
 
 def findAverageSentence(filePath, delimiters, minLength):
+
     file = open(filePath)
     textInFile = file.read()
 
     words = textInFile.split(" ")
-    # print(words)
 
-    sentences = textInFile.split(".")
-    sentences.remove("")
+    # Using regex for delimiters
+    delimiters = '[{0}]'.format(delimiters.replace(" ", ""))
+    sentences = re.split(delimiters, textInFile)
 
-    # print(sentences)
+    # Remove empty strings from the sentences list
+    while '' in sentences:
+        sentences.remove('')
+
+    # Remove extra whitespace from beginning and end of each sentence
+    for i in range(len(sentences)):
+        sentences[i] = sentences[i].strip()
+
+    print(sentences)
+
     if (len(sentences) > 0):
         aveSentenceLength = len(words) / len(sentences)
     else:
@@ -20,12 +31,20 @@ def findAverageSentence(filePath, delimiters, minLength):
     return round(aveSentenceLength, 0)
 
 
-userFile = input("Enter the file path to the .txt file you wish to analyze.")
-userDelimeters = input("Enter the characters (punctuation) that you want to be sentence "
-                       "delimiters separated by spaces")
-userDelimeters = userDelimeters.split(" ")
-minLength = eval(input("Enter the minimum length of a word (must be a positive integer)"))
-if (minLength < 1):
-    minLength = 1
+def main():
 
-print("The average sentence length is", findAverageSentence(userFile, userDelimeters, minLength))
+    userFile = input("Enter the file path to the .txt file you wish to analyze.")
+
+    userDelimeters = input("Enter the characters (punctuation) that you want to be sentence "
+                           "delimiters separated by spaces")
+
+    minLength = eval(input("Enter the minimum length of a word (must be a positive integer)"))
+
+    if (minLength < 1):
+        minLength = 1
+
+    print("The average sentence length is", findAverageSentence(userFile, userDelimeters, minLength))
+
+
+if __name__ == '__main__':
+    main()
