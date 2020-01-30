661360c5e86d304ac2cbe7ec4898c50422ea2af1
Dar Vince Ocarol <DarVince.Ocarol@RJUHSD.INT>
2020-01-30 12:01:45 -0800

    Read text files

--- SentenceLength.py
+++ SentenceLength.py
@@ -1,1 +1,15 @@
-print ("this is a test")
+
+
+directory = input("Where is the file located? ")
+
+try:
+    textFile = open(directory, "r")
+    print(textFile.read())
+
+except:
+    print("Please enter a valid directory")
+    openFile()
+
+
+
+
