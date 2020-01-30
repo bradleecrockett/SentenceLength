6ca7e39e93a628ea1d272c3303c1e4b95f44ea99
annanhoang <anna@hoang.io>
2020-01-30 11:44:29 -0800

    Set aveSentenceLength to round(Ave...) to be returned

---
+++ SentenceLength.py
@@ -0,0 +1,33 @@
+import os
+
+
+def findAverageSentence(filePath, delimiters, minLength):
+    file = open(filePath)
+    textInFile = file.read()
+
+    words = textInFile.split(" ")
+    print(words)
+
+    sentences = textInFile.split(".")
+    sentences.remove("")
+
+    print(sentences)
+    if (len(sentences) > 0):
+        aveSentenceLength = len(words) / len(sentences)
+    else:
+        aveSentenceLength = words
+
+    aveSentenceLength = round(aveSentenceLength)
+
+    return aveSentenceLength
+
+
+userFile = input("Enter the file path to the .txt file you wish to analyze.")
+userDelimeters = input("Enter the characters (punctuation) that you want to be sentence "
+                       "delimiters separated by spaces")
+userDelimeters = userDelimeters.split(" ")
+minLength = eval(input("Enter the minimum length of a word (must be a positive integer)"))
+if (minLength < 1):
+    minLength = 1
+
+print("The average sentence length is", findAverageSentence(userFile, userDelimeters, minLength))
