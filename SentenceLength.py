1b21f4c7828ba9fb9add399be027176491855dcf
MatthewSheppard1 <34612037+MatthewSheppard1@users.noreply.github.com>
2020-01-30 11:56:11 -0800

    added list collection

--- SentenceLength.py
+++ SentenceLength.py
@@ -3,19 +3,33 @@ def averageLen(delimeter, directory, minLength):
     text = open(directory)
     textInFile = text.read()
     words = textInFile.split(" ")
-
     wordsToRemove = []
-
+    lengths = []
     for i in words:
         if len(i) - 1 < minLength:
             wordsToRemove.append(i)
 
     for i in wordsToRemove:
         words.remove(i)
+    num = 0
+    for i in words:
+        d = False
+        for i2 in delimeter:
+            if(i[len(i)-1] == i2):
+                d = True
+        if(d):
+            num += 1
+            lengths.append(num)
+            num = 0
+        else:
+            num += 1
+    print(lengths)
 
 def main():
-    delim = input("Enter delimeter: ")
+    delim = input("Enter delimeters(seperated by spaces): ")
+    delim = delim.split()
     path = input("Enter the directory for text file: ")
-    len = input("Enter the min length for a word: ")
+    len = int(input("Enter the min length for a word: "))
 
-    averageLen(deli)
+    averageLen(delim,path,len)
+main()
