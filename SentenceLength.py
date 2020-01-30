8184e955cfa5716c81058d6941c35f58a5cb4e5d
kateywillick9 <kateywillick@gmail.com>
2020-01-30 12:05:50 -0800

    test

--- SentenceLength.py
+++ SentenceLength.py
@@ -1,1 +1,65 @@
 print ("this is a test")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fileName = input("enter fileName")
+file = open(fileName, "r")
+for line in file:
+    print (line)
+
+file.close()
