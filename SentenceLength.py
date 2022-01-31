9422f9b2d3c33bde652ca6cfb508298fbf2a52ee
Khang Tran <khangtran@Khangs-MacBook-Pro.local>
2022-01-31 10:34:51 -0800

    added sentence length function

--- SentenceLength.py
+++ SentenceLength.py
@@ -19,6 +19,26 @@ def findAverageSentence(filePath, delimiters, minLength):
 
     return round(aveSentenceLength, 0)
 
+def findAverageWord(filePath, delimiters):
+    file = open(filePath)
+    textInFile = file.read()
+
+    words = textInFile.split(" ")
+    # print(words)
+
+    sentences = textInFile.split(".")
+    sentences.remove("")
+    letters = len(textInFile)
+    letters = letters - textInFile.count(" ")
+    letters = letters - textInFile.count(".")
+    if(words == 0):
+        averageWordLength = 0
+    else:
+        averageWordLength = letters/len(words)
+
+    return round(averageWordLength, 2)
+
+
 
 def main():
     userFile = input("Enter the file path to the .txt file you wish to analyze.")
@@ -30,6 +50,7 @@ def main():
         minLength = 1
 
     print("The average sentence length is", findAverageSentence(userFile, userDelimeters, minLength))
+    print("The average letter per words is:", findAverageWord(userFile, userDelimeters))
 
 if __name__ == "__main__":
     main()
