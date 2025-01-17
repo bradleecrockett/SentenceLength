ea7b80cdcd20b7d8179024357caf393abb22a139
Bluejustice397 <bluejustice397@gmail.com>
2025-01-17 13:26:52 -0800

    Added function to handle misentered files

--- SentenceLength.py
+++ SentenceLength.py
@@ -28,6 +28,14 @@ def findAverageSentence(filePath, delimiters, minLength):
 
 def main():
     userFile = input("Enter the file path to the .txt file you wish to analyze.\n")
+    isFile = os.path.isfile(userFile)
+    while isFile != True:
+        print("Enter a valid file")
+        userFile = input("Enter the file path to the .txt file you wish to analyze.\n")
+        isFile = os.path.isfile(userFile)
+        
+
+
     userDelimeters = input("Enter the characters (punctuation) that you want to be sentence delimiters separated by spaces.\n")
     userDelimeters = userDelimeters.split(" ")
     minLength = input("Enter the minimum length of a word (must be a positive integer).\n")
